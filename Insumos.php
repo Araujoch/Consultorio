ee09996fadbb03a8e062ee0683cf6aec19e867cf
root <root@localhost.localdomain>
2023-07-19 15:25:45 -0400

    Formulario Insumos 50%

--- Insumos.php
+++ Insumos.php
@@ -3,91 +3,91 @@
 <head>
     <title>Formulario</title>
     <link rel="stylesheet" href="style.css">
-	<script>
-    function generarMedicamentos() {
-      var medicamentos = ["Medicamento 1", "Medicamento 2", "Medicamento 3", "Medicamento 4", "Medicamento 5", "Medicamento 6", "Medicamento 7", "Medicamento 8", "Medicamento 9", "Medicamento 10"];
-      var cantidad = 10;
-      var medicamentosAleatorios = [];
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 
-      // Generar 10 medicamentos aleatorios
-      while (medicamentosAleatorios.length < cantidad) {
-        var randomIndex = Math.floor(Math.random() * medicamentos.length);
-        var medicamento = medicamentos[randomIndex];
 
-        // Evitar duplicados
-        if (!medicamentosAleatorios.includes(medicamento)) {
-          medicamentosAleatorios.push(medicamento);
-        }
-      }
 
-      // Mostrar los medicamentos en la página
-      var medicamentosDiv = document.getElementById("medicamentos");
-      medicamentosDiv.innerHTML = "";
-
-      for (var i = 0; i < medicamentosAleatorios.length; i++) {
-        var medicamentoCheckbox = document.createElement("input");
-        medicamentoCheckbox.type = "checkbox";
-        medicamentoCheckbox.name = "medicamento" + (i + 1);
-        medicamentosDiv.appendChild(medicamentoCheckbox);
-
-        var medicamentoLabel = document.createElement("label");
-        medicamentoLabel.htmlFor = medicamentoCheckbox.name;
-        medicamentoLabel.appendChild(document.createTextNode(medicamentosAleatorios[i]));
-        medicamentosDiv.appendChild(medicamentoLabel);
-
-        medicamentosDiv.appendChild(document.createElement("br"));
-      }
-    }
-  </script>
 </head>
 	<body>
-			<div class = "container">
-
-				<div>
-					<h1>Cargo de Insumos Pabellon</h1>
-				</div>
-				<div class = "container-row1">
-					<label for="">Nombre del paciente</label>
-					<input type="text" readonly>
-					<label for="">Rut</label>
-					<input type="text" readonly>
-					<label for="">Fecha</label>
-					<input type="text" readonly>
-				</div>
-				<div class = "container-row2">
-					<label for="">Cirujano</label>
-					<input type="text"readonly>
-					<label for="">Diagnostico/Cirugia</label>
-					<input type="text">
-					<label for="">Pabellonera/o</label>
-					<input type="text">
-				</div class = "container-check">
-
-					<div class = "container-check-medicamentos">
-						<h1> Medicamentos</h1>
-						<input type="checkbox" name="medicamneto1" id="">
-						<input type="checkbox" name="medicamneto2" id="">
-						<input type="checkbox" name="medicamneto3" id="">
-						<input type="checkbox" name="medicamneto4" id="">
-						<input type="checkbox" name="medicamneto5" id="">
-						<input type="checkbox" name="medicamneto6" id="">
-						<input type="checkbox" name="medicamneto7" id="">
-						<input type="checkbox" name="medicamneto8" id="">
-						<input type="checkbox" name="medicamneto" id="">
-						<input type="checkbox" name="medicamneto" id="">
-						<input type="checkbox" name="medicamneto" id="">
-						<input type="checkbox" name="medicamneto" id="">
-					</div>
-
-				<!-- <div class = "container-check">
-					<div class = "container-check-medicamentos">
-						<h1>Generador de Medicamentos Aleatorios</h1>
-						<button onclick="generarMedicamentos()">Generar Medicamentos</button>
-						<div id="medicamentos"></div>
-					</div>
-
-				</div> -->
+	<div class = "container">
+		<div class="row">
+			<div class="col-md-3 overflow-auto" style="max-height: 400px;">
+				<h3>Medicamentos</h3>
+				<h3>Insumos</h3>
+				<h3>Suturas</h3>
 			</div>
+			
+			<div class = "col-md-10 bg-secondary-subtle" >  
+				<form class="row g-3">
+					<div class="col-md-5">
+						<label for="name_paciente" class="form-label">Nombre del Paciente</label>
+						<input type="text" class="form-control" id="name_paciente">
+					</div>
+					<div class="col-md-4">
+						<label for="rut" class="form-label">Rut</label>
+						<input type="password" class="form-control" id="rut">
+					</div>
+					<div class="col-md-3">
+						<label for="fecha" class="form-label">Fecha</label>
+						<input type="date" class="form-control" id="fecha">
+					</div>
+					<div class="col-md-5">
+						<label for="cirujano" class="form-label">Cirujano</label>
+						<input type="text" class="form-control" id="cirujano">
+					</div>
+					<div class="col-md-4">
+						<label for="diagnostico" class="form-label">Diagnostico/Cirugia</label>
+						<input type="text" class="form-control" id="diagnostico">
+					</div>
+					<div class="col-md-3">
+						<label for="pabellonero" class="form-label">Pabellonera/o</label>
+						<input type="text" class="form-control" id="pabellonero">
+					</div>
+					<div class="container border border-secondary mt-2">
+						<div class="row">
+							<h3>Medicamentos</h3>
+						</div>	
+						<div class="row ">
+						<div class="col-6 form-check">
+								<input class="form-check-input" type="checkbox" name="flexRadioDefault" id="flexRadioDefault1">
+								<label class="form-check-label text-break" for="flexRadioDefault1">
+									Paracetamol
+								</label>
+							</div>
+							<div class="col-6 form-check">
+								<input class="form-check-input" type="checkbox" name="flexRadioDefault" id="flexRadioDefault1">
+								<label class="form-check-label text-break" for="flexRadioDefault1">
+									Geniolito
+								</label>
+							</div>
+							
+						</div>
+						<div class="row">
+							<div class="col-6 form-check">
+								<input class="form-check-input" type="checkbox" name="flexRadioDefault" id="flexRadioDefault1">
+								<label class="form-check-label text-break" for="flexRadioDefault1">
+									Dipirona
+								</label>
+							</div>
+							<div class="col-6 form-check">
+								<input class="form-check-input" type="checkbox" name="flexRadioDefault" id="flexRadioDefault1">
+								<label class="form-check-label text-break" for="flexRadioDefault1">
+									kitadol
+								</label>
+							</div>
+							
+						</div>
+					</div>
+					<div class="col-12">
+						<button type="submit" class="btn btn-primary">Sign in</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>	
+
 
 	</body>
+	
 </html>
